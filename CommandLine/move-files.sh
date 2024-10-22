3f975fbfb91e1af861d5950452970aaa394230b6
dingjiayuan-oss <dingjiayuan13@gmail.com>
2024-10-22 14:07:19 +0800

    Update move-files.sh

---
+++ CommandLine/move-files.sh
@@ -0,0 +1,11 @@
+//移动父文件夹下的 a.txt 到当前文件夹
+
+//移动当前文件夹下的 tmp 文件夹到 dir 文件夹内
+
+//将当前文件夹下的 b.txt 重命名为 c.txt
+
+//将 test 文件夹重命名为 test1 文件夹
+$ mv a.txt temp/
+$ mv tmp dir1/
+$ mv b.txt c.txt
+$ mv test test1
